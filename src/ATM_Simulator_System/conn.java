971dae35b6ff8b3c71858c4b7891eea16368db43
Anish Gupta <anishgupta675@gmail.com>
2021-12-19 15:41:31 +0530

    config files

--- src/ATM_Simulator_System/conn.java
+++ src/ATM_Simulator_System/conn.java
@@ -1,17 +1,27 @@
-package ATM_Simulator_System;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package atm_simulator_system;
 
+/**
+ *
+ * @author C9 Hyper
+ */
 import java.sql.*;
 
 public class conn {
-	Connection c;
-	Statement s;
-	public conn() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			c = DriverManager.getConnection("jdbc:mysql:///project6","root","thedarkphoenix1171");
-			s = c.createStatement();
-		} catch(Exception e) {
-			System.out.println(e);
-		}
-	}
+
+    Connection c;
+    Statement s;
+
+    public conn() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///project6", "root", "thedarkphoenix1171");
+            s = c.createStatement();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
